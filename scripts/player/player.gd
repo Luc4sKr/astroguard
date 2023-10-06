173d738a7a770c1f1023dc091fb787a5062498a7
Luc4sKr <lucaskreuch77@gmail.com>
2023-10-06 20:26:13 -0300

    reload time

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,11 +3,20 @@ extends Sprite2D
 
 var bullet = preload("res://scenes/bullet.tscn")
 
+var is_reloaded = true
+
 func _process(delta):
 	var mouse_position = get_viewport().get_mouse_position()
 	var direction = (mouse_position - global_position).normalized()
 	var angle = atan2(direction.y, direction.x)
 	rotation_degrees = angle * 180 / PI
 	
-	if Input.is_action_just_pressed("shoot") and Global.parent_node_creation != null:
-		Global.instance_node(bullet, global_position, Global.parent_node_creation)
+	if Input.is_action_pressed("shoot") and is_reloaded:
+		if Global.parent_node_creation != null:
+			Global.instance_node(bullet, global_position, Global.parent_node_creation)
+			
+			is_reloaded = false
+			$reload_time.start()
+
+func _on_reload_time_timeout():
+	is_reloaded = true 
