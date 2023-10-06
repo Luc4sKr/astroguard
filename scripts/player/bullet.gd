d07e55952eff1d7df34e7ffed888b62e2a7700f6
Luc4sKr <lucaskreuch77@gmail.com>
2023-10-05 23:09:24 -0300

    arrumando rotação do tiro

--- scripts/player/bullet.gd
+++ scripts/player/bullet.gd
@@ -4,10 +4,14 @@ var mov = Vector2(1, 0)
 var speed = 250
 var single_direction = true
 
+func _ready():
+	pass
+
 func _process(delta: float) -> void:
 	if single_direction:
 		look_at(get_global_mouse_position())
 		single_direction = false
+	
 	global_position += mov.rotated(rotation) * speed * delta
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
